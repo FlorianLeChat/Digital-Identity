d4da6a55808b3d296e99dce0f496ffae71ea8fc6
Brahim Lamjarad <lamjarad@gmail.com>
2023-01-13 10:32:44 +0100

    Début vue teacher avec réglage de différents problèmes

---
+++ migrations/Version20221212105429.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221212105429 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE presence (id INT AUTO_INCREMENT NOT NULL, stud_presence_id INT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, token VARCHAR(255) NOT NULL, INDEX IDX_6977C7A5E0F315C4 (stud_presence_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE presence ADD CONSTRAINT FK_6977C7A5E0F315C4 FOREIGN KEY (stud_presence_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE presence DROP FOREIGN KEY FK_6977C7A5E0F315C4');
+        $this->addSql('DROP TABLE presence');
+        $this->addSql('ALTER TABLE user CHANGE roles roles JSON DEFAULT NULL');
+    }
+}
