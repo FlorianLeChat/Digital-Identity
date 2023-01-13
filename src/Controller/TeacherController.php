57d96a9183b160b91273e195b0d4a64c226428d3
Brahim Lamjarad <lamjarad@gmail.com>
2023-01-13 13:51:11 +0100

    Partie Générer un code de la vue teacher terminée

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -6,8 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+
+
 class TeacherController extends AbstractController
-{
+{   
     #[Route('/portalTeacher', name: 'app_teacher')]
     public function index(): Response
     {
@@ -15,4 +20,48 @@ class TeacherController extends AbstractController
             'controller_name' => 'TeacherController',
         ]);
     }
+
+    /**
+    * @Route("/generate-code", name="generate_code")
+    */
+    public function generateCode()
+    {
+        $code = rand(1000, 9999);
+        return $this->render('teacher/code.html.twig', [
+            'code' => $code,
+        ]);
+    }
+
+
+
+    #[Route('/portalTeacher', name: 'app_teacher')]
+    public function new(Request $request)
+{
+    $form = $this->createFormBuilder()
+        ->add('name', TextType::class)
+        ->add('save', SubmitType::class, array('label' => 'Create Task'))
+        ->getForm();
+    return $this->render('teacher/index.html.twig', array(
+        'form' => $form->createView(),
+    ));
+}
+
+    public function create(Request $request)
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $task = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+            return $this->redirectToRoute('task_success');
+        }
+        return $this->render('teacher/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
 }
